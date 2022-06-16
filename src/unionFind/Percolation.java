56dbf5e99a6339e0e7313dfb8e3f852c1a6aecf8
Joyce Chen <hsini.huang721@gmail.com>
2022-06-15 23:11:39 -0700

    finished percolation data structure

--- src/unionFind/Percolation.java
+++ src/unionFind/Percolation.java
@@ -13,70 +13,75 @@ public class Percolation {
     /*
         creates n-by-n grid, with all sites initially blocked
         value of -1 means "blocked"
-        use n*n + 1 as the "virtual top root"
-        use n*n + 2 as the "virtual bottom root"
+        use n*n as the "virtual top root"
+        use n*n + 1 as the "virtual bottom root"
      */
     public Percolation(int n){
         numOpen = 0;
-        size = n;
         grid = new int [n + 1][n + 1];
+        size = n + 1;
         for (int i = 0; i <= n; i++){
             for (int j = 0; j <= n; j++){
                 grid[i][j] = -1;
             }
         }
         uf = new WeightedQuickUnionUF(n * n + 2);
-        topRoot = n * n + 1;
-        bottomRoot = n * n + 2;
+        topRoot = n * n;
+        bottomRoot = n * n + 1;
     }
 
     /*
         makes it easier to retrieve index for uf structure.
-        basically transforms [i][j] to just 1 index.
+        makes 1,1 at 0; 1,2 at 1...
      */
     private int getIndex(int row, int col){
         row--;
         col--;
-        return row * grid[row].length + col;
+        return row * (size - 1) + col;
     }
 
     /*
         opens the site (row, col) if it is not open already
+        use value 0 to signify opening
      */
     public void open(int row, int col){
         int currNode = getIndex(row, col);
-        grid[row][col] = 0; //if there's no opening around
+        grid[row][col] = 0;
         numOpen++;
-        if (uf.find(currNode) == )
-        if (row == 1){
+        if (row == 1) {
             uf.union(currNode, topRoot);
         }
-        else if (isOpen(row - 1, col)){
-            uf.union(currNode, getIndex(row - 1, col));
+        else if (row == size - 1){
+            uf.union(currNode, bottomRoot);
         }
 
-        if(isOpen(row + 1, col)){
+        //check top of current position
+        if (row - 1 > 0 && isOpen(row - 1, col)){
+            uf.union(currNode, getIndex(row - 1, col));
+        }
+        //check bottom of current position
+        if(row + 1 < size && isOpen(row + 1, col)){
             uf.union(currNode, getIndex(row + 1, col));
         }
-        if (isOpen(row, col - 1)){
+        //check left of current position
+        if (col - 1 > 0 && isOpen(row, col - 1)){
             uf.union(currNode, getIndex(row, col - 1));
         }
-        if (isOpen(row, col + 1)){
+        //check right of current position
+        if (col + 1 < size && isOpen(row, col + 1)){
             uf.union(currNode, getIndex(row, col + 1));
         }
     }
 
     /*
         is the site (row, col) open?
-        value -1 as "blocked" indication
+        -1 = "blocked"
      */
     public boolean isOpen(int row, int col){
         return grid[row][col] != -1;
     }
 
-    /*
-        is the site (row, col) full?
-     */
+    //is the site (row, col) full?
     public boolean isFull(int row, int col){
         int root = uf.find(getIndex(row, col));
         return root == topRoot;
@@ -89,12 +94,25 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates(){
-
+        return uf.find(topRoot) == uf.find(bottomRoot);
     }
 
     // test client (optional)
     public static void main(String[] args){
-
+        Percolation grid = new Percolation(8);
+        grid.open(1,1);
+        grid.open(1, 2);
+        grid.open(2, 2);
+        grid.open(3,2);
+        grid.open(3, 3);
+        grid.open(4,3);
+        grid.open(5,3);
+        grid.open(5,4);
+        grid.open(5,5);
+        grid.open(6,5);
+        grid.open(7,5);
+        grid.open(8,5);
+        System.out.println(grid.percolates());
     }
 }
 
