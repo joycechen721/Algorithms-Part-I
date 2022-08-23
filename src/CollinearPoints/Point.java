1d1726cd8b55585160fef528d74fb7b077f47b0f
Joyce Chen <joyce.chen721@gmail.com>
2022-08-22 23:19:53 -0700

    start implementing point

--- src/CollinearPoints/Point.java
+++ src/CollinearPoints/Point.java
@@ -4,8 +4,10 @@ import java.util.Comparator;
 
 public class Point {
     // constructs the point (x, y)
+    private int x, y;
     public Point(int x, int y){
-
+        this.x = x;
+        this.y = y;
     }
 
     // draws this point
@@ -20,17 +22,32 @@ public class Point {
 
     // string representation
     public String toString(){
+        return "(" + this.getX() + ", " + this.getY() + ")";
+    }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     // compare two points by y-coordinates, breaking ties by x-coordinates
     public int compareTo(Point that){
-
+        if(that.getY() > this.getY()) return -1;
+        else if(that.getY() < this.getY()) return 1;
+        else{
+            if(that.getX() > this.getX()) return -1;
+            else if(that.getX() < this.getX()) return 1;
+        }
+        return 0;
     }
 
     // the slope between this point and that point
     public double slopeTo(Point that){
-
+        double slope = (double)(this.getY() - that.getY()) / (this.getX() - that.getX());
+        return slope;
     }
 
     // compare two points by slopes they make with this point
